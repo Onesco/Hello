13f1f530d56d97b413103c7c7a83088034313448
Onesco <39714372+Onesco@users.noreply.github.com>
2019-03-30 00:24:05 +0100

    Create hallo.js

---
+++ hallo.js
@@ -0,0 +1,9 @@
+var a = 2;
+var b = 3;
+var c = a+b;
+var summation=(num1, num2)=>{
+return c= num1+num2
+};
+if(summation > c){
+Console.log('summed number is High than'+ c  + 'and it's value is:'+ summation)
+};
