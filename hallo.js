f324f9f925c734b888efbe3e0d0529818ae3cf4c
Onesco <39714372+Onesco@users.noreply.github.com>
2019-04-02 20:41:45 +0100

    Delete hallo.js

--- hallo.js
+++
@@ -1,9 +0,0 @@
-var a = 2;
-var b = 3;
-var c = a+b;
-var summation=(num1, num2)=>{
-return c= num1+num2
-};
-if(summation > c){
-Console.log('summed number is High than'+ c  + 'and it's value is:'+ summation)
-};
